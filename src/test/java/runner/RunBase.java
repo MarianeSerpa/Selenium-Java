c5e861aa30e6f4c4853af112d0fe2b4dfe6245b3
MarianeSerpa <mariane.serpa_2701@hotmail.com>
2025-01-04 03:12:30 -0300

    Atualização de pom

--- src/test/java/runner/RunBase.java
+++ src/test/java/runner/RunBase.java
@@ -1,9 +1,10 @@
 package runner;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
 import java.util.concurrent.TimeUnit;
 
 public class RunBase {
@@ -20,24 +21,34 @@ public class RunBase {
             driver.quit();
         }
 
+        // Configuração do WebDriverManager para garantir que a versão correta do driver seja utilizada
+        WebDriverManager.chromedriver().setup(); // Essa linha garante que o WebDriverManager busque o driver correto
+
         switch (browser) {
             case "chrome":
                 driver = new ChromeDriver();
                 break;
+
+            case "chrome-ci":
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--headless");
+                driver = new ChromeDriver(chromeOptions);
+                break;
+
             case "firefox":
                 driver = new FirefoxDriver();
                 break;
+
             case "edge":
                 throw new IllegalArgumentException("Edge ainda nao suportado");
+
             default:
-                throw new IllegalArgumentException("Navegador não encontrado! Passe um navegador existente: chrome, forefox ou edge.");
+                throw new IllegalArgumentException("Navegador não encontrado! Passe um navegador existente: chrome, firefox ou edge.");
         }
 
         if(driver != null){
             driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         }
-
         return driver;
     }
-
 }
